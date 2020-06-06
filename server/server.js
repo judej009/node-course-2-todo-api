39bb28ec496764eed1abf83b7cc1a8e1e098b6dd
jude <jude@test.com>
2020-06-06 15:01:51 +0400

    Add POST /user/login route

--- server/server.js
+++ server/server.js
@@ -105,7 +105,6 @@ app.post('/users', (req, res) => {
     })
 });
 
-
 app.get('/users/me',authenticate, (req, res) =>  {
     var token = req.header('x-auth');
     
@@ -116,6 +115,18 @@ app.get('/users/me',authenticate, (req, res) =>  {
         res.send(user);
     }).catch((e) => {
         res.status(401).send();
+    });
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
